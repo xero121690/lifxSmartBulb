6c23acae868236951ea29d66d70abe5ced63ef3c
Moniushko <44934043+Moniushko@users.noreply.github.com>
2018-12-13 00:30:28 -0800

    Added a Navbar up top

---
+++ lifx/src/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
+class AboutUs extends Component {
+	render() {
+		return (
+		<div>
+			<h2>HI</h2>
+				<p>dasdasda</p>
+		</div>
+		)
+	}
+}
+
+export default AboutUs;
---
+++ lifx/src/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
+class Home extends Component {
+	render() {
+		return (
+		<div>
+			<h2>HI</h2>
+				<p>dasdasda</p>
+		</div>
+		)
+	}
+}
+
+export default Home;
